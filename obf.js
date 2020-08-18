02c3f095b3ab9501f3168dc8e4ca45e9c134c456
Maoundis <51264133+ikbal-hanafi@users.noreply.github.com>
2020-08-19 03:43:19 +0800

    x

--- obf.js
+++ obf.js
@@ -17,30 +17,30 @@ j=(c,s)=>{
 #from xiuz code 
 #https://ikbal-hanafi.github.io/sbobf
 
-${v1}(){
-  ${v2}=$(cat $0);
-  ${v2}=\${#${v2}}
-  while true; do 
-    if(( \${#${v2}} == 2 )); then
-      break
-    fi
-    ${v2}=$(( $${v2} / 2 ))
-  done
-  ${h("echo")} $${v2}
-}
+${v1}=$(${h("cat")} $0);
+  
+${v2}=\${#${v1}}
+while true; do 
+  if(( \${#${v2}} == 2 )); then
+     break
+  fi
+  ${v2}=$(( $${v2} / 2 ))
+done
 
-${h("eval")} $(${h("echo")} -e $(sed "s/x00/x$(${v1})/" <<< ${c}))
-  `);
+${h("eval")} $(${h("echo")} ${h("-e")} $(${h("sed")} "s/x00/x$${v2}/g" <<< ${c}))
+
+`);
   
 },
-obf=async (i,c=15)=>{
-  var a=`${h("eval")} $(${h("echo")} ${h(btoa(i))} | ${h("base64")} -d)`, a = h(a, false);
+obf=async (i,c=500)=>{
+  var a=`${h("eval")} $(${h("echo")} ${h(btoa(i))} | ${h("base64")} ${h("-d")})`, a = h(a, false);
   var b=j(a,c).length;
   
   while(true){
-    if(b.toString().length === 2) break;
+    if(b.toString().length === 2) {
+      i=b;break;
+    }
     b=Math.floor(b / 2);
-    
   }
   
   
